ed33350aea57c1b08522c1f9377c61b62f51060f
Yepes Correa <yepescorreaj@student.douglascollege.ca>
2024-07-23 14:35:40 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// require('dotenv').config();
+
+const port = process.env.PORT || 3000;
+
+// Create a Schema object---------------------------------------------------------------------
+
+const StudentSchema = new mongoose.Schema(
+  {
+    myName: String,
+    mySID: String,
+  },
+  { collection: "s24students" }
+);
+
+// Create a Model object----------------------------------------------------------------------------
+
+const StudentModel = mongoose.model("student", StudentSchema);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/form.html");
+});
+
+app.post("/", async (req, res) => {
+  // get the data from the form--------------------------------------------------------------------------
+  const myuri = req.body.myuri;
+
+  // connect to the database and log the connection---------------------------------------------------
+  await mongoose.connect(myuri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  // add the data to the database-----------------------------------------------------------------------
+  const student = new StudentModel({
+    myName: "Daniel Yepes",
+    mySID: "300382464",
+  });
+  await student.save();
+
+  // send a response to the user-----------------------------------------------------------------------
+  res.send(`<h1>Document  Added</h1>`);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
